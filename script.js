2774b3fe949dd1d4ad41ddb7453c56160f345af1
KongTi <95029870+KongTi@users.noreply.github.com>
2022-07-08 19:23:53 +0900

    Add files via upload
    JS관련 버튼 동작

---
+++ script.js
@@ -0,0 +1,14 @@
+function clickbutton() {
+  alert("구하러 왔구나!!");
+  alert("[아니 나도 잡혔어]");
+  alert("당신도 대학원에 사로 잡혀버렸습니다!");
+}
+
+function anw() {
+  var chat = prompt("[공부 영화 운동 박사과정]중에서 선택하세요");
+  if (chat === "운동") {
+    alert("정답이네요");
+  } else {
+    alert("틀렸습니다");
+  }
+}
